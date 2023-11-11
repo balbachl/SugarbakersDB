b46d462f8b08dc8a2c88ee52618874090a297517
Lisa Balbach <lisajbalbach@gmail.com>
2023-11-11 02:23:21 -0500

    Update ItemsOnOrder.sql

--- ItemsOnOrder.sql
+++ ItemsOnOrder.sql
@@ -1,10 +1,11 @@
-﻿ CREATE TABLE ItemsonOrder(
-	[OrdersID] int NOT NULL,
+ CREATE TABLE ItemsonOrder(
+	[ItemID] int NOT NULL,
 	[ProductsID] int NOT NULL,
 	Quantity int NULL,
 	UnitPrice money NULL,
 	LineItemTotal AS UnitPrice * Quantity,
 	ShipDate datetime NULL,
- CONSTRAINT PK_itemsonorder PRIMARY KEY CLUSTERED ([OrdersID],[ProductsID]),
- CONSTRAINT FK_OrdersItems FOREIGN KEY(OrdersID) REFERENCES Orders(OrdersID),
- CONSTRAINT FK_ProductItems FOREIGN KEY(ProductsID) REFERENCES Products(ProductsID));
+        [OrdersID] INT NOT NULL, 
+    CONSTRAINT FK_OrdersItems FOREIGN KEY([OrdersID]) REFERENCES Orders(OrdersID),
+    CONSTRAINT FK_ProductItems FOREIGN KEY(ProductsID) REFERENCES Products(ProductsID), 
+    CONSTRAINT [PK_ItemsonOrder] PRIMARY KEY ([ItemID]));
